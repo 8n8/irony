b2474034f8c0fd9876cf06b0b85171344be26679
True Ghiassi <true@ghiassitrio.co.uk>
2023-02-17 21:17:58 +0000

    Formatting

--- src/Irony.elm
+++ src/Irony.elm
@@ -1,8 +1,9 @@
 module Irony exposing (convert)
 
+
 convert : String -> Result String String
 convert elm =
     """export function Main() {
   return <h1>Hello</h1>;
 }"""
-    |> Ok
+        |> Ok
--- tests/Tests.elm
+++ tests/Tests.elm
@@ -1,10 +1,10 @@
 module Tests exposing (..)
 
+import Dict
 import Expect exposing (Expectation)
 import Fuzz exposing (Fuzzer, int, list, string)
-import Test exposing (..)
-import Dict
 import Irony
+import Test exposing (..)
 
 
 suite : Test
@@ -13,9 +13,10 @@ suite =
         \_ ->
             Expect.equal (Irony.convert elm) (Ok react)
 
+
 elm : String
 elm =
-      """module Main exposing (main)
+    """module Main exposing (main)
 
 import Html
 
@@ -24,8 +25,9 @@ main =
     Html.text "hi"
 """
 
+
 react : String
 react =
-      """export function Main() {
+    """export function Main() {
   return <h1>Hello</h1>;
 }"""

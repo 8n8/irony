c33993dd78bcb860ccf079040362a3f08d35b8af
True Ghiassi <true@ghiassitrio.co.uk>
2023-02-18 12:51:19 +0000

    Removed Debug.todo

--- src/Irony.elm
+++ src/Irony.elm
@@ -68,10 +68,10 @@ exportsToReact : Module -> String
 exportsToReact module_ =
     case module_ of
         PortModule _ ->
-            Debug.todo "PortModule not implemented yet"
+            "PortModule not implemented yet"
 
         EffectModule _ ->
-            Debug.todo "EffectModule not implemented yet"
+            "EffectModule not implemented yet"
 
         NormalModule { exposingList } ->
             case unwrap exposingList of
@@ -79,7 +79,7 @@ exportsToReact module_ =
                     ""
 
                 All _ ->
-                    Debug.todo "Exposing all not implemented yet"
+                    "Exposing all not implemented yet"
 
                 Explicit oneOrMore ->
                     [ "export {"
@@ -96,13 +96,13 @@ exportToReact expose =
             name
 
         InfixExpose _ ->
-            Debug.todo "InfixExpose not implemented"
+            "InfixExpose not implemented"
 
         TypeOrAliasExpose _ ->
-            Debug.todo "TypeOrAliasExpose not implemented"
+            "TypeOrAliasExpose not implemented"
 
         TypeExpose _ ->
-            Debug.todo "TypeExpose not implemented"
+            "TypeExpose not implemented"
 
 
 declarationToReact : Node Declaration -> String
@@ -112,19 +112,19 @@ declarationToReact (Node _ declaration) =
             functionToReact function
 
         AliasDeclaration _ ->
-            Debug.todo "Alias declaration not implemented"
+            "Alias declaration not implemented"
 
         CustomTypeDeclaration _ ->
-            Debug.todo "Custom type declaration not implemented"
+            "Custom type declaration not implemented"
 
         PortDeclaration _ ->
-            Debug.todo "Port declaration not implemented"
+            "Port declaration not implemented"
 
         InfixDeclaration _ ->
-            Debug.todo "Infix declaration not implemented"
+            "Infix declaration not implemented"
 
         Destructuring _ _ ->
-            Debug.todo "Destructuring not implemented"
+            "Destructuring not implemented"
 
 
 unwrap : Node a -> a
@@ -155,61 +155,61 @@ expressionToReact expression =
             String.join "." (moduleName ++ [ functionName ])
 
         IfBlock _ _ _ ->
-            Debug.todo "IfBlock not implemented"
+            "IfBlock not implemented"
 
         PrefixOperator _ ->
-            Debug.todo "PrefixOperator not implemented"
+            "PrefixOperator not implemented"
 
         Operator _ ->
-            Debug.todo "Operator not implemented"
+            "Operator not implemented"
 
         Integer _ ->
-            Debug.todo "Integer not implemented"
+            "Integer not implemented"
 
         Hex _ ->
-            Debug.todo "Hex not implemented"
+            "Hex not implemented"
 
         Floatable _ ->
-            Debug.todo "Floatable not implemented"
+            "Floatable not implemented"
 
         Negation _ ->
-            Debug.todo "Negation not implemented"
+            "Negation not implemented"
 
         Literal literal ->
             String.concat [ "\"", literal, "\"" ]
 
         CharLiteral _ ->
-            Debug.todo "CharLiteral not implemented"
+            "CharLiteral not implemented"
 
         TupledExpression _ ->
-            Debug.todo "TupledExpression not implemented"
+            "TupledExpression not implemented"
 
         LetExpression _ ->
-            Debug.todo "LetExpression not implemented"
+            "LetExpression not implemented"
 
         CaseExpression _ ->
-            Debug.todo "CaseExpression not implemented"
+            "CaseExpression not implemented"
 
         LambdaExpression _ ->
-            Debug.todo "LambdaExpression not implemented"
+            "LambdaExpression not implemented"
 
         RecordExpr _ ->
-            Debug.todo "RecordExpr not implemented"
+            "RecordExpr not implemented"
 
         ListExpr _ ->
-            Debug.todo "ListExpr not implemented"
+            "ListExpr not implemented"
 
         RecordAccess _ _ ->
-            Debug.todo "RecordAccess not implemented"
+            "RecordAccess not implemented"
 
         RecordAccessFunction _ ->
-            Debug.todo "RecordAccessFunction not implemented"
+            "RecordAccessFunction not implemented"
 
         RecordUpdateExpression _ _ ->
-            Debug.todo "RecordUpdateExpression not implemented"
+            "RecordUpdateExpression not implemented"
 
         GLSLExpression _ ->
-            Debug.todo "GLSLExpression not implemented"
+            "GLSLExpression not implemented"
 
         ParenthesizedExpression parenthesized ->
             [ "("
@@ -219,7 +219,7 @@ expressionToReact expression =
                 |> String.concat
 
         UnitExpr ->
-            Debug.todo "UnitExpr not implemented"
+            "UnitExpr not implemented"
 
         Application app ->
             List.map unwrap app
@@ -227,7 +227,7 @@ expressionToReact expression =
                 |> applicationToReact
 
         OperatorApplication _ _ _ _ ->
-            Debug.todo "OperatorApplication not implemented"
+            "OperatorApplication not implemented"
 
 
 applicationToReact : List String -> String

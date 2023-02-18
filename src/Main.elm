c7d3f6aa0dd99efa8f27d7bbd1fa02383e221174
True Ghiassi <true@ghiassitrio.co.uk>
2023-02-18 13:09:51 +0000

    Tidying up the UI

--- src/Main.elm
+++ src/Main.elm
@@ -15,8 +15,7 @@ main =
 import Html
 
 main =
-    Html.text "hello"
-"""
+    Html.text "hello\""""
         , view = view
         , update = update
         }
@@ -25,25 +24,26 @@ main =
 view : String -> Html String
 view elm =
     [ header
+    , Element.paragraph [] [ Element.text "So far it handles simple imports, exports, function declarations, function calls and string literals." ]
     , textBox elm
     , typescript elm
     ]
         |> Element.column
             [ Element.width Element.fill
-            , Element.spacing 16
+            , Element.spacing 24
             , Element.padding 8
             ]
-        |> Element.layout []
+        |> Element.layout [ Font.family [ Font.sansSerif ] ]
 
 
 header =
-    [ Element.text "Convert Elm to React"
+    [ Element.text "Convert Elm to Typescript and React"
         |> Element.el
             [ Font.size 32
             ]
-    , Element.text "A work in progress demo"
+    , Element.text "A work-in-progress demo"
     ]
-        |> Element.column []
+        |> Element.column [ Element.spacing 8 ]
 
 
 update : String -> String -> String
@@ -52,24 +52,26 @@ update msg _ =
 
 
 typescript elm =
-    let
-        _ =
-            Debug.log "elm" elm
-    in
     case Irony.convert elm of
         Err err ->
             Element.text ("There was an error: " ++ err)
 
         Ok ts ->
-            Element.text ts
+            [ Element.text "Here is the corresponding Typescript and React code:"
+            , Element.text ts
+                |> Element.el [ Font.family [ Font.monospace ] ]
+            ]
+                |> Element.column [ Element.spacing 16 ]
 
 
 textBox elm =
     Input.multiline
-        [ Element.width Element.fill ]
+        [ Element.width Element.fill
+        , Font.family [ Font.monospace ]
+        ]
         { onChange = \x -> x
         , text = elm
         , placeholder = Nothing
-        , label = Input.labelAbove [] (Element.text "Type your Elm code here:")
+        , label = Input.labelAbove [ Font.family [ Font.sansSerif ] ] (Element.text "Type your Elm code here:")
         , spellcheck = False
         }
